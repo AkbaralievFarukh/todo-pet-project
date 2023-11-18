d88946864d7ae32f7e8ed3cd366d8db08063b6d0
Farukh <akbfarukh@gmail.com>
2023-11-18 21:04:09 +0600

    modifided

---
+++ script.js
@@ -0,0 +1,14 @@
+const addDoBtn = document.querySelector('#add-do__btn')
+const tasksList = document.querySelector('.tasks-list')
+
+const doItem = {}
+
+let allDo = () => {
+    tasksList.innerHTML = `
+
+    `
+}
+
+const addDo = () => {
+
+}
